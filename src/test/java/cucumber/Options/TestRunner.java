6bb16afb40b4a034a7079e56e10ec76f4c61d2d0
omerfarukekuklu <omerfarukekuklu@gmail.com>
2021-10-08 02:04:31 +0300

    Added Html reporting for cucumber.

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = {"step_definitions"}, tags = "@Regression and not @DeletePlace")
+@CucumberOptions(features = "src/test/java/features", glue = {"step_definitions"},plugin ="json:target/jsonReports/cucumber-report.json", tags = "@Regression and not @DeletePlace")
 public class TestRunner {
 //, tags = "@AddPlace or @DeletePlace"
 }
